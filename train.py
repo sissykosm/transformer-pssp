4d0f0fc6358a26beacf3fb1e730916d4beca0f7e
Sissy Kosma <sissykosm@gmail.com>
2019-09-30 02:33:34 +0300

    test focal loss

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def cal_performance(pred, gold, smoothing=False):
 
     gold = gold.ne(Constants.PAD)
     accuracy2 = get_acc(pred.tolist(), gold.tolist())
-    print(loss)
+    
     return loss, n_correct, accuracy2
 
 
