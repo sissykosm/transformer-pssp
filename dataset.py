aa49dc260edf4d78dcccd279bfaa411e16275b0e
Sissy Kosma <sissykosm@gmail.com>
2019-10-28 02:38:17 +0200

    add tickets

--- dataset.py
+++ dataset.py
@@ -24,12 +24,11 @@ def collate_fn_x(insts, sp_insts):
         for inst in insts])
 
     max_len2 = max(len(inst) for inst in sp_insts)
-    (sp_insts) = sp_insts
     batch_sp = np.array([[
         inst.tolist() + [Constants.PAD] * (max_len2 - len(inst))
         for inst in sp]
         for sp in sp_insts])
-    
+    print(batch_sp)
     batch_pos = np.array([
         [pos_i+1 if w_i != Constants.PAD else 0
          for pos_i, w_i in enumerate(inst)] for inst in batch_seq])
